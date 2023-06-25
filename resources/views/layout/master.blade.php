60258d9812f86b7e81217e6b7eb6f8b39eed9810
mhkhaldi95 <35767463+mhkhaldi95@users.noreply.github.com>
2023-06-26 01:25:27 +0300

    .

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -13,12 +13,15 @@ License: For each use you must have a valid license purchased only from above li
 {{--<html lang="en">--}}
 <html lang="{{app()->getLocale() == 'ar'?'ar':'en'}}" dir="{{app()->getLocale() == 'ar'?'rtl':'ltr'}}">
 <!--begin::Head-->
-<head><base href="">
+<head>
+    <base href="">
     @include('layout.head')
 </head>
 <!--end::Head-->
 <!--begin::Body-->
-<body id="kt_body" class="header-fixed header-tablet-and-mobile-fixed toolbar-enabled toolbar-fixed aside-enabled aside-fixed" style="--kt-toolbar-height:55px;--kt-toolbar-height-tablet-and-mobile:55px">
+<body id="kt_body"
+      class="header-fixed header-tablet-and-mobile-fixed toolbar-enabled toolbar-fixed aside-enabled aside-fixed"
+      style="--kt-toolbar-height:55px;--kt-toolbar-height-tablet-and-mobile:55px">
 <!--begin::Main-->
 <!--begin::Root-->
 <div class="d-flex flex-column flex-root">
@@ -28,15 +31,15 @@ License: For each use you must have a valid license purchased only from above li
         <!--begin::Aside-->
         @if(auth()->user()->role == \App\Constants\Enum::PLACE)
             @include('layout.place_aside')
-            @else
+        @else
             @include('layout.aside')
-            @endif
+        @endif
 
         <!--end::Aside-->
         <!--begin::Wrapper-->
         <div class="wrapper d-flex flex-column flex-row-fluid" id="kt_wrapper">
             <!--begin::Header-->
-           @include('layout.header')
+            @include('layout.header')
             <!--end::Header-->
             <!--begin::Content-->
             <div class="content d-flex flex-column flex-column-fluid" id="kt_content">
@@ -45,7 +48,9 @@ License: For each use you must have a valid license purchased only from above li
                     <!--begin::Container-->
                     <div id="kt_toolbar_container" class="container-fluid d-flex flex-stack">
                         <!--begin::Page title-->
-                        <div data-kt-swapper="true" data-kt-swapper-mode="prepend" data-kt-swapper-parent="{default: '#kt_content_container', 'lg': '#kt_toolbar_container'}" class="page-title d-flex align-items-center flex-wrap me-3 mb-5 mb-lg-0">
+                        <div data-kt-swapper="true" data-kt-swapper-mode="prepend"
+                             data-kt-swapper-parent="{default: '#kt_content_container', 'lg': '#kt_toolbar_container'}"
+                             class="page-title d-flex align-items-center flex-wrap me-3 mb-5 mb-lg-0">
                             <!--begin::Title-->
                             <h1 class="d-flex align-items-center text-dark fw-bolder fs-3 my-1">{{$page_title}}</h1>
                             <!--end::Title-->
@@ -62,11 +67,11 @@ License: For each use you must have a valid license purchased only from above li
                     <!--end::Container-->
                 </div>
                 <!--end::Toolbar-->
-               @yield('content')
+                @yield('content')
             </div>
             <!--end::Content-->
             <!--begin::Footer-->
-           @include('layout.footer')
+            @include('layout.footer')
             <!--end::Footer-->
 
 
@@ -152,18 +157,32 @@ License: For each use you must have a valid license purchased only from above li
     $(document).ready(function () {
 
 
+        function showNotification() {
+            new Notification('طلبية جديد', {
+                body: 'تفقد الاشعارات'
+            });
+        }
+
         $('#kt_aside_toggle').click()
 
         @if(auth()->user()->role != \App\Constants\Enum::PLACE)
-        setInterval(function (){
+        setInterval(function () {
             axios.get('{{route('notifications.header')}}').then(function (response) {
-              $('#kt_topbar_notifications_1').html(response.data.data.notifications)
+                $('#kt_topbar_notifications_1').html(response.data.data.notifications)
 
-                if(response.data.data.count_notifications > 0){
+                if (response.data.data.count_notifications > 0) {
 
-                    new Notification('طلبية جديد', {
-                        body: 'تفقد الاشعارات'
-                    });
+                    if (Notification.permission !== 'granted') {
+                        Notification.requestPermission().then(function (permission) {
+                            if (permission === 'granted') {
+                                showNotification();
+                            } else {
+                                console.log('Notification permission denied.');
+                            }
+                        });
+                    } else {
+                        showNotification();
+                    }
                     $('#count_notifications').html(response.data.data.count_notifications)
                     $('#count_notifications').addClass('count_notifications')
 
@@ -171,7 +190,7 @@ License: For each use you must have a valid license purchased only from above li
 
                 }
             })
-        },30000)
+        }, 30000)
 
         @endif
     });
